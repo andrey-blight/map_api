18ad86033d386ad87546c451fa12a6f21906ae92
Kizhinov-Andrey <kzihinov05@gmail.com>
2022-02-07 20:09:07 +0300

    Add first task solution

--- Scripts/constans.py
+++ Scripts/constans.py
@@ -1,2 +1,3 @@
 HEIGHT = 450
 WIDTH = 600
+SPN = [0.1, 0.1]  # approximation
--- Scripts/main.py
+++ Scripts/main.py
@@ -1,52 +1,47 @@
+from PyDesing.first_task_design import Ui_MainWindow
+from constans import *
+
 import os
 import sys
-
 import requests
-from PyQt5 import uic
-from PyQt5.QtGui import QPixmap
+
+from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 
-class Example(QMainWindow):
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        # self.getImage()
-        # self.initUI()
-        uic.loadUi(r'../Design/first_task_design.ui', self)  # Загружаем дизайн
+        self.setupUi(self)
+        self.btn_show.clicked.connect(self.show_map)
 
-    def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
-        response = requests.get(map_request)
+    def show_map(self):
+        # Собираем параметры для запроса к StaticMapsAPI:
+        map_params = {
+            "ll": ",".join(str(s) for s in [self.dsp_long.value(), self.dsp_width.value()]),
+            "spn": ",".join(str(s) for s in SPN),
+            "l": "map"}
 
-        if not response:
-            print("Ошибка выполнения запроса:")
-            print(map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
-            sys.exit(1)
+        map_api_server = "http://static-maps.yandex.ru/1.x/"
+        response = requests.get(map_api_server, params=map_params)
 
-        # Запишем полученное изображение в файл.
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
-
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
-        ## Изображение
-        self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
+        self.lbl_map.setPixmap(QPixmap(self.map_file))
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
 
 
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec())
