56350da3c2abab7f472c90e439a4809f2e3bc641
Kizhinov-Andrey <kzihinov05@gmail.com>
2022-02-08 16:09:31 +0300

    changes to second task

--- Scripts/main.py
+++ Scripts/main.py
@@ -14,7 +14,7 @@ from PyQt5.Qt import *
 class YandexMap(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.zoom = 12  # map zoom
+        self.zoom = 0.1  # map zoom
         self.setupUi(self)  # load design
         self.show_map()  # show map with start cords
         self.btn_show.clicked.connect(self.show_map)
@@ -22,8 +22,8 @@ class YandexMap(QMainWindow, Ui_MainWindow):
     def show_map(self):
         map_params = {
             "ll": ",".join(str(s) for s in [self.dsp_long.value(), self.dsp_width.value()]),
-            "z": str(self.zoom),
             "size": f"{WIDTH},{HEIGHT}",
+            "spn": f"{self.zoom},{self.zoom}",
             "l": "map"}
         response = requests.get(MAP_API_SERVER, params=map_params)
         pixmap = QPixmap()  # container for map
@@ -31,11 +31,13 @@ class YandexMap(QMainWindow, Ui_MainWindow):
         self.lbl_map.setPixmap(pixmap)  # load map to label
 
     def keyPressEvent(self, event: QKeyEvent):
+        # we will change value to 50%
         if event.key() == Qt.Key_PageUp:
-            self.zoom = min(17, self.zoom + 1)
+            # max value is (90 - |width|) because this is the edge of the map + 10 for zooming another part of the map
+            self.zoom = min(90 - abs(self.dsp_width.value()) + 10, self.zoom + self.zoom * 0.5)
             self.show_map()  # change zoom and repaint map
         if event.key() == Qt.Key_PageDown:
-            self.zoom = max(self.zoom - 1, 0)
+            self.zoom = max(self.zoom - self.zoom * 0.5, 0.0001)
             self.show_map()  # change zoom and repaint map
 
 
